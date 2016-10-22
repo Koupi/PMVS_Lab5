88e4e1e6d5b731292917506055ae8c54d5de8c45
Koupi <sasha3849@yandex.ru>
2016-10-22 14:21:55 +0300

    File with tests added.

---
+++ test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+insmod calculator.ko
+`dmesg | grep -m 1 'mknod /dev/first' | awk -F "'" {'print $2'}`
+`dmesg | grep -m 1 'mknod /dev/second' | awk -F "'" {'print $2'}`
+`dmesg | grep -m 1 'mknod /dev/operand' | awk -F "'" {'print $2'}`
+
+function check {
+    echo $1 > /dev/first
+    echo $2 > /dev/operand
+    echo $3 > /dev/second
+    if [ "$4" == "`cat /proc/result`" ]; then
+        echo "True!"
+    else
+        echo "False!"
+    fi
+}
+
+check 3 + 2 "  5"
+check 20 + 2 " 22"
+check 77 + 33 "110"
+
+check 20 - 18 "  2"
+check 44 - 33 " 11"
+check 2 - 1 "  1"
+
+check 55 / 11 "  5"
+check 33 / 11 "  3"
+check 21 / 7 "  3"
+
+rmmod calculator
+rm /dev/operand
+rm /dev/first
+rm /dev/second
+
